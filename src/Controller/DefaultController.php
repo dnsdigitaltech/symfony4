d97e58425be1c6406bf9ae5c956d8302aafef45b
dnsdigitaltech <dnssites@gmail.com>
2020-09-10 16:52:15 -0300

    Primeiros Passos com Twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,27 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('index.html.twig');
+        $posts = [
+            [
+                'id' => 1,
+                'title' => 'Post 1',
+                'created_at' => '2020-10-09 16:35:55'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Post 2',
+                'created_at' => '2020-10-09 16:35:55'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Post 3',
+                'created_at' => '2020-10-09 16:35:55'
+            ],
+        ];
+        return $this->render('index.html.twig', [
+            'title' => 'Postagem Teste',
+            'posts' => $posts,
+        ]);
     }
 
 }

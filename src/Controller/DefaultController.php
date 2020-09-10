77047aef59d99fa85a7ba728d36d853b53379c47
dnsdigitaltech <dnssites@gmail.com>
2020-09-10 15:56:31 -0300

    Rotas via Annotations

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController
 {
+    /**
+     * @Route("/")
+     */
     public function index()
     {
         return new Response('Primeiro retorno no Synfony!');
